2b4b6540160bc108433a2f122df7ee31839cffd0
Rhabia-Fodra <44965438+Rhabia-Fodra@users.noreply.github.com>
2018-11-12 10:23:20 +0100

    Add files via upload

---
+++ readme.m
@@ -0,0 +1,38 @@
+%  FACIAL EXPRESSION RECOGNITION
+%
+% Copy all files in Matlab current directory and type "adafacialexp" on
+% Matlab command window.
+% 
+% First, select an input image clicking on "Select image".
+% Then you can
+%   - add this image to database (click on "Add selected image to database" button)
+%   - perform gender recognition (click on "Facial Expression Recognition" button)
+%     Note: If you want to perform gender recognition database has to include 
+%     at least one image.
+%  If you choose to add image to database, you have to specify its facial
+%  expression (an integer number between 0 and 6).
+%
+% 
+% 
+% FUNCTIONS
+%
+% Select image:                                  read the input image
+%
+% Add selected image to database:                the input image is added to database and will be used for training
+%
+%
+% Facial Expression Recognition:                 facial expression identification. The selected input image is processed
+%
+% Database Info:                                 show database
+%
+% Delete Database:                               remove database from the current directory
+%
+% Program Info:                                  show informations about this software
+%
+%
+% Source code for Facial Expression Recognition: how to obtain the complete source code
+%
+% Exit:                                          quit program
+%
+%
+%
---
+++ sourcecode.m
@@ -0,0 +1,6 @@
+%FACIAL EXPRESSION RECOGNITION
+%
+%Feel Free to visit us:
+%http://matlab-recognition-code.com/facial-expression-recognition-matlab-code/
+% 
+% For more informations please email me rhabia-fodra@matlab-recognition-code.com
